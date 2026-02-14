27ed65c1256261cbd2029e64a435d6da3d1c8e9a
ChoteX <dachitchotashvili@gmail.com>
2026-02-14 15:02:28 +0400

    github pages support

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/Furniture-shop/' : '/',
   plugins: [react()],
   server: {
     proxy: {
@@ -10,4 +11,4 @@ export default defineConfig({
       '/static': 'http://localhost:3000',
     },
   },
-})
+}))
